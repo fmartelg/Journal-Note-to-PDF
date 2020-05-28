d1d59487f82c18c2a201053a6b53374b6a55cf7b
fmartelg <fmg229@nyu.edu>
2020-05-28 00:25:29 -0400

    Created working print_jnt function

--- jnt_to_pdf.py
+++ jnt_to_pdf.py
@@ -1,43 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-
 """
 Convert all .jnt files in my machine to .pdf bc Windows Journal is deprecated.
 1. Identify all .jnt files in my documents
 2. Print each one to PDF as "name-jnt.pdf" in same location
-3. Delete .jnt version
+3. Delete .jnt file
 """
 
 ###########
 # Libraries
 ###########
-
-from ahk import AHK
+# TODO: Remove all unneeded libraries
+# from ahk import AHK # ahk run does not take args!!!
 import os
+from pathlib import Path
 import subprocess
 
 ###########
 # Functions
 ###########
-def print_jnt(jntPath):
+def print_jnt(docPath):
     """
-    Input: A string with the location of a jnt file
-    Ouput: A pdf printout of the file, in the same location, with the same name 
+    Input: A string with the absolute path to a jnt file
+    Ouput: A pdf printout of the file, saved in the last location JournalNote sprinted to PDF manually. Set it manually to current directory!!!
     """
-    # TODO: bring over the .ahk function 
-    return XYZ
-ahk = AHK()
-# ahk run does not take args!!!
-# Will need to use powershell
+    subprocess.run(['powershell.exe', './jnt_to_pdf.ahk', docPath])
+    return docPath
 
-var1 = 'var11'
-var2 = 'var22'
-subprocess.run(['powershell.exe', './test.ahk', var1, var2])
+# TODO: Print file from different location, copy PDF to said location, and delete it in working directory.
+# TODO: Function that identifies all jnt files in an input directory
+# TODO: Make sure PDF does not already exist before printing, else the Journal Note prompt to replace will break the program.
 
 ###########
 # Inputs
 ###########
+jntPath = "C:\\Users\\Fernando\\Documents\\scripts\\print_journal_notes\\Note1.jnt"
 
 ###########
 # Outputs
 ###########
+print_jnt(jntPath)
