9922aca1bc1bdaac6bf45a1303c9e308c30a4bc9
fmartelg <fmg229@nyu.edu>
2020-06-04 23:13:42 -0400

    Completed movePDF function

--- jnt_to_pdf.py
+++ jnt_to_pdf.py
@@ -1,12 +1,14 @@
 """
 Convert all .jnt files in my machine to .pdf bc Windows Journal is deprecated.
 1. Identify all .jnt files in my documents
-2. Print each one to PDF as "name-jnt.pdf" in same location
-3. Delete .jnt file
+2. Print each one to PDF as "filename-jnt.pdf" in same location
+3. Delete .jnt file afger printing to PDF (optional)
 """
 
 # DONE: Provide output file name as argument
-# TODO: Print file from a different folder, copy printed PDF to original location, and delete PDF in working directory.
+# DONE: Print file from a different folder
+# DONE: copy printed PDF to original location
+# TODO: Delete PDF in working directory.
 # TODO: Function that identifies all jnt files in an input directory
 # TODO: Make sure PDF does not already exist before printing, else the Journal Note prompt to replace will break the program.
 
@@ -16,30 +18,47 @@ Convert all .jnt files in my machine to .pdf bc Windows Journal is deprecated.
 # TODO: Remove all unneeded libraries
 # from ahk import AHK # ahk run does not take args!!!
 import os
+import shutil
 from pathlib import Path
 import subprocess
 
 ###########
 # Functions
 ###########
+
 def print_jnt(inputPath, outputFilename):
     """
     Inputs: 
       - A string with the absolute path to a jnt file
-      - A string with the output filename (not path)
+      - A string with the output filename only (i.e. excluding path)
     Ouput: 
       - A pdf printout of the file, saved in the last location JournalNote sprinted to PDF manually. Need to set it manually to current directory before running for first time.
     """
     subprocess.run(['powershell.exe', './jnt_to_pdf.ahk', inputPath, outputFilename])
     return inputPath
 
+def move_pdf(sourceFileName, destinationPath):
+    """
+    After printing a Journal Note to PDF in the default printer directory, this function copies the PDF to the location of the Journal Note.
+    Inputs:
+      - Name of source file (assumed is located in default printing directory)
+      - Destination filepath
+    Outputs:
+      - None
+    """
+    filename = sourceFileName
+    destination = destinationPath
+    shutil.move(filename,destination) 
+
 ###########
 # Inputs
 ###########
-jntPath = "C:\\Users\\Fernando\\Documents\\scripts\\print_journal_notes\\Note1.jnt"
+jntPath = "C:\\Users\\Fernando\\Documents\\a\\Note-a.jnt"
+pdfFilename = Path(jntPath).stem + '-jnt.pdf' # Name PDF as "filename-jnt.pdf"
+pdfDestination = Path(Path(jntPath).parent).joinpath(pdfFilename)
 
 ###########
 # Outputs
 ###########
-pdfFilename = Path(jntPath).stem + '-jnt' # Name PDF as "name-jnt.pdf"
 print_jnt(jntPath, pdfFilename)
+move_pdf(pdfFilename, pdfDestination)
