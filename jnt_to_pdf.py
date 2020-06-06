4079082013a5de35aca9183e7cd171716d04e480
fmartelg <fmg229@nyu.edu>
2020-06-05 22:52:22 -0400

    Function to find all jnt files in root dir

--- jnt_to_pdf.py
+++ jnt_to_pdf.py
@@ -8,9 +8,10 @@ Convert all .jnt files in my machine to .pdf bc Windows Journal is deprecated.
 # DONE: Provide output file name as argument
 # DONE: Print file from a different folder
 # DONE: copy printed PDF to original location
-# TODO: Function that identifies all jnt files in an input directory
+# DONE: Function that identifies all jnt files in an input directory
 # TODO: Make sure PDF does not already exist before printing, else the
 #       Journal Note prompt to replace will break the program.
+# TODO: Check that there is a PDF version of each jnt in each directory
 # TODO: Delete PDF in working directory.
 
 ###########
@@ -58,9 +59,27 @@ def move_pdf(sourceFileName, destinationPath):
     shutil.move(filename, destination)
 
 
+def find_jnt_paths(rootDirectory):
+  """
+  Searches in the root directory and all subdirectories iteratigvely to find all .jnt files.
+  Inputs:
+    - A root directory
+  Outputs:
+    - A list of full paths to found files. 
+  """
+  list_jnt_paths = []
+  rootDir = rootDirectory
+  for root, dirs, files in os.walk(rootDir):
+    for file in files:
+        if file.endswith(".jnt"):
+             list_jnt_paths.append(os.path.join(root, file))
+  return list_jnt_paths
+
+
 ###########
 # Inputs
 ###########
+jntRoot = "C:\\Users\\Fernando\\Documents\\a"
 jntPath = "C:\\Users\\Fernando\\Documents\\a\\Note-a.jnt"
 pdfFilename = Path(jntPath).stem + '-jnt.pdf' # Name PDF as "filename-jnt.pdf"
 pdfDestination = Path(Path(jntPath).parent).joinpath(pdfFilename)
@@ -68,5 +87,6 @@ pdfDestination = Path(Path(jntPath).parent).joinpath(pdfFilename)
 ###########
 # Outputs
 ###########
+jntPathList = find_jnt_paths(jntRoot)
 print_jnt(jntPath, pdfFilename)
 move_pdf(pdfFilename, pdfDestination)
